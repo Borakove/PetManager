3395c26cccef055ed4cc7c8038a037f8990b0ea7
Matheus <majorsaints@gmail.com>
2025-05-05 14:43:31 -0300

    Versão completa

---
+++ src/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author relat
+ */
+public class main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaLogin().setVisible(true);
+            }
+        });
+    }
+}
+
